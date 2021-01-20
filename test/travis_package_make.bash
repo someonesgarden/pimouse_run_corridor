ebbda9ce4fc310fe7bc9d269e43ad0e1c5a7b17e
someonesgarden <d@someonesgarden.org>
2021-01-20 08:09:00 +0000

    some test codes

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,7 +1,12 @@
 #!/bin/bash -xve
 
 #sync and make
-rsync -av ./ ~/catkin_ws/src/pimouse_ros/
+rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+#rsync -av ./ ~/catkin_ws/src/pimouse_ros/
+
+cd ~/catkin_ws/src/
+git clone --depth=1 https://github.com/someonesgarden/pimouse_ros.git
+
 cd ~/catkin_ws
 catkin_make
 
